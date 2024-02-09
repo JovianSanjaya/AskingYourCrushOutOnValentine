e8ce9ee4ffab3879627ae8b0c85976f0edb21c4d
jovian <JOVIAN14.22@ICHAT.SP.EDU.SG>
2024-02-10 01:46:09 +0800

    first

---
+++ script.js
@@ -0,0 +1,29 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Yay, see you on valentine!";
+  gif.src =
+    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+});
+
+array = ['are you sure? :(', 'Im going to be sad:(' , 'Really?:(','Think about it:(']
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+
+  const randomIndex = Math.floor(Math.random() * array.length);
+  noBtn.innerHTML = array[randomIndex];
+
+});
